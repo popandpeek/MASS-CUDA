087bfeb1ce875b0dee30174f89ffec81de83e1c1
Ben Pittman <benpitts@uw.edu>
2020-12-15 13:45:56 -0800

    Minor changes for multiple gpu.

--- src/cudaUtil.h
+++ src/cudaUtil.h
@@ -16,7 +16,7 @@
 
 namespace mass {
 
-#define BLOCK_SIZE 24  // max threads per block
+#define BLOCK_SIZE 32  // max threads per block
 #define H2D cudaMemcpyHostToDevice
 #define D2H cudaMemcpyDeviceToHost
 #define CERR
